027d47fb4213f03e7a9e86258942bd2c724bae69
Vladimir Krupin <vladimir.krupin133@gmail.com>
2020-04-07 19:46:46 +0400

    build

---
+++ dist/precache-manifest.876b5ab47aa314173330c3744a81ddb9.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "8ec3f2c7bb4fdccdc4ae6296ff74a618",
+    "url": "/img/theme/gifkawood.png"
+  },
+  {
+    "revision": "b3cad28bc72d72c0e250",
+    "url": "/js/chunk-vendors.72980267.js"
+  },
+  {
+    "revision": "cdb32e4bed5ffdf6e1cf023b35da2e53",
+    "url": "/img/theme/skill.png"
+  },
+  {
+    "revision": "2b1b9fd3f33b0a1b3378cdee9b947c35",
+    "url": "/logo.png"
+  },
+  {
+    "revision": "136f5e75da845e602bf10e5f03d6c9ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b14a93871f130a08e9a2",
+    "url": "/js/app.f108076a.js"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "5e7e669f9438d2162a32df27a2537b5a",
+    "url": "/img/brand/blue.svg"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "3c59d91a14251ebea437a5a6230214d9",
+    "url": "/img/brand/white.svg"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b14a93871f130a08e9a2",
+    "url": "/css/app.db6a555f.css"
+  }
+];
